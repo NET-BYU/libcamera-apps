9fba2c0d29e1759b0d86628203a339a7c9572406
Philip Lundrigan <lundrigan@byu.edu>
2023-01-04 15:44:55 -0700

    Add new function

--- apps/libcamera_ecen224.cpp
+++ apps/libcamera_ecen224.cpp
@@ -26,8 +26,37 @@ public:
 // TODO:
 // 	x Remove app
 //	x How not to parse options
-// 	- Save to memory not file
+// 	x Save to memory not file
+//	x Figure out how to save data to a image
 // 	- Figure out how to compile this as a library
+
+struct ImageHeader
+{
+	uint32_t size = sizeof(ImageHeader);
+	uint32_t width;
+	int32_t height;
+	uint16_t planes = 1;
+	uint16_t bitcount = 24;
+	uint32_t compression = 0;
+	uint32_t imagesize = 0;
+	uint32_t xpels = 100000;
+	uint32_t ypels = 100000;
+	uint32_t clrused = 0;
+	uint32_t clrimportant = 0;
+};
+static_assert(sizeof(ImageHeader) == 40, "ImageHeader size wrong");
+
+struct FileHeader
+{
+	uint16_t dummy; // 2 dummy bytes so that our uint32_ts line up
+	uint8_t type1 = 'B';
+	uint8_t type2 = 'M';
+	uint32_t filesize;
+	uint16_t reserved1 = 0;
+	uint16_t reserved2 = 0;
+	uint32_t offset = sizeof(FileHeader) - 2 + sizeof(ImageHeader);
+};
+static_assert(sizeof(FileHeader) == 16, "FileHeader size wrong");
 
 LibcameraJpegApp app;
 
@@ -36,15 +65,62 @@ static void camera_init()
 	StillOptions *options = app.GetOptions();
 	options->Parse(1, {NULL});
 
+	unsigned int still_flags = LibcameraApp::FLAG_STILL_NONE;
+	still_flags |= LibcameraApp::FLAG_STILL_RGB;
+
 	app.OpenCamera();
-	app.ConfigureStill();
+	app.ConfigureStill(still_flags);
 	app.StartCamera();
 }
 
-static void camera_get_still(std::string filename)
+void save_to_bmp(uint8_t* mem, unsigned int width, unsigned int height, unsigned int stride,
+			  std::string const &filename)
 {
-	StillOptions const *options = app.GetOptions();
+	FILE *fp = filename == "-" ? stdout : fopen(filename.c_str(), "wb");
 
+	if (fp == NULL)
+		throw std::runtime_error("failed to open file " + filename);
+
+	try
+	{
+		unsigned int line = width * 3;
+		unsigned int pitch = (line + 3) & ~3; // lines are multiples of 4 bytes
+		unsigned int pad = pitch - line;
+		uint8_t padding[3] = {};
+		uint8_t *ptr = mem;
+
+		FileHeader file_header;
+		ImageHeader image_header;
+		file_header.filesize = file_header.offset + height * pitch;
+		image_header.width = width;
+		image_header.height = -height; // make image come out the right way up
+
+		// Don't write the file header's 2 dummy bytes
+		if (fwrite((uint8_t *)&file_header + 2, sizeof(file_header) - 2, 1, fp) != 1 ||
+			fwrite(&image_header, sizeof(image_header), 1, fp) != 1)
+			throw std::runtime_error("failed to write BMP file");
+
+		for (unsigned int i = 0; i < height; i++, ptr += stride)
+		{
+			if (fwrite(ptr, line, 1, fp) != 1 || (pad != 0 && fwrite(padding, pad, 1, fp) != 1))
+				throw std::runtime_error("failed to write BMP file, row " + std::to_string(i));
+		}
+
+		LOG(2, "Wrote " << file_header.filesize << " bytes to BMP file");
+
+		if (fp != stdout)
+			fclose(fp);
+	}
+	catch (std::exception const &e)
+	{
+		if (fp && fp != stdout)
+			fclose(fp);
+		throw;
+	}
+}
+
+static int camera_get_still(uint8_t* buf, unsigned int* width, unsigned int* height, unsigned int* stride)
+{
 	for (;;)
 	{
 		LibcameraApp::Msg msg = app.Wait();
@@ -58,22 +134,34 @@ static void camera_get_still(std::string filename)
 
 		if (msg.type == LibcameraApp::MsgType::Quit)
 		{
-			return;
+			return -1;
 		}
 
 		if (msg.type != LibcameraApp::MsgType::RequestComplete)
 		{
-			throw std::runtime_error("unrecognised message!");
+			return -1;
 		}
-
-		LOG(1, "Still capture image received");
 
 		Stream *stream = app.StillStream();
 		StreamInfo info = app.GetStreamInfo(stream);
+
+		if (info.pixel_format != libcamera::formats::RGB888)
+		{
+			fprintf(stderr, "Pixel format should be RGB\n");
+			return -1;
+		}
+
 		CompletedRequestPtr &payload = std::get<CompletedRequestPtr>(msg.payload);
 		const std::vector<libcamera::Span<uint8_t>> mem = app.Mmap(payload->buffers[stream]);
-		jpeg_save(mem, info, payload->metadata, filename, app.CameraId(), options);
-		return;
+		libcamera::Span<uint8_t> image_data = mem[0];
+
+		// Update parameters
+		memcpy(buf, image_data.data(), image_data.size());
+		*width = info.width;
+		*height = info.height;
+		*stride = info.stride;
+
+		return 0;
 	}
 }
 
@@ -84,26 +172,25 @@ static void camera_exit()
 
 int main(int argc, char *argv[])
 {
-	try
-	{
-		std::chrono::milliseconds duration(500);
+	uint8_t *buf = (uint8_t *)malloc(15116544);
+	unsigned int width;
+	unsigned int height;
+	unsigned int stride;
 
-		camera_init();
-		camera_get_still("frame1.jpg");
-		std::this_thread::sleep_for(duration);
-		camera_get_still("frame2.jpg");
-		std::this_thread::sleep_for(duration);
-		camera_get_still("frame3.jpg");
-		std::this_thread::sleep_for(duration);
-		camera_get_still("frame4.jpg");
-		std::this_thread::sleep_for(duration);
-		camera_get_still("frame5.jpg");
-		camera_exit();
+	// Start camera
+	camera_init();
+
+	// Get a still
+	if(camera_get_still(buf, &width, &height, &stride) == -1) {
+		fprintf(stderr, "Unable to get still image...\n");
+		exit(1);
 	}
-	catch (std::exception const &e)
-	{
-		LOG_ERROR("ERROR: *** " << e.what() << " ***");
-		return -1;
-	}
+
+	// Save the still to an file
+	save_to_bmp(buf, width, height, stride, "foobar.bmp");
+
+	// Exit camera
+	camera_exit();
+
 	return 0;
 }
